c4d0d664fd493cd443980f269c52717413c00d4b
Thinh Vo <ducthinh2410@gmail.com>
2020-04-30 22:32:34 +0300

    Add User type

---
+++ src/user/user.type.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: string;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    isVerified?: boolean;
+    bio?: string;
+    userType?: string;
+    profileImageUrl: string;
+    token?: string;
+  }
